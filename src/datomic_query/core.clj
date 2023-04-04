bf14e066f96e3bbc1c77f7fd3dbccd45c6232594
Rafael Lopes <rafael84@gmail.com>
2023-04-03 23:01:27 -0300

    wip

--- src/datomic_query/core.clj
+++ src/datomic_query/core.clj
@@ -2,58 +2,38 @@
   (:require
    [datomic.api :as d]))
 
-(def schema [{:db/ident :task/title
-              :db/valueType :db.type/string
-              :db/cardinality :db.cardinality/one
-              :db/doc "The title of a task in a todo list."}
+(def schema [{:db/ident       :team/name
+              :db/unique      :db.unique/identity
+              :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/one}
 
-             {:db/ident :task/due-date
-              :db/valueType :db.type/instant
-              :db/cardinality :db.cardinality/one
-              :db/doc "The due date of a task in a todo list."}
+             {:db/ident       :team/tasks
+              :db/valueType   :db.type/ref
+              :db/cardinality :db.cardinality/many}
 
-             {:db/ident :task/completed
-              :db/valueType :db.type/boolean
-              :db/cardinality :db.cardinality/one
-              :db/doc "The completion status of a task in a todo list."}
+             {:db/ident :task/title     :db/valueType :db.type/string  :db/cardinality :db.cardinality/one}
+             {:db/ident :task/due-date  :db/valueType :db.type/instant :db/cardinality :db.cardinality/one}
+             {:db/ident :task/completed :db/valueType :db.type/boolean :db/cardinality :db.cardinality/one}
+             {:db/ident :task/priority  :db/valueType :db.type/keyword :db/cardinality :db.cardinality/one}])
 
-             {:db/ident :task/priority
-              :db/valueType :db.type/keyword
-              :db/cardinality :db.cardinality/one
-              :db/doc "The priority of a task in a todo list."}
+(def teams [{:team/name "Alpha"}
+            {:team/name "Beta"}])
 
-             {:db/ident :task.priority/low
-              :db/valueType :db.type/keyword
-              :db/cardinality :db.cardinality/one
-              :db/doc "Low priority for a task in a todo list."}
-
-             {:db/ident :task.priority/medium
-              :db/valueType :db.type/keyword
-              :db/cardinality :db.cardinality/one
-              :db/doc "Medium priority for a task in a todo list."}
-
-             {:db/ident :task.priority/high
-              :db/valueType :db.type/keyword
-              :db/cardinality :db.cardinality/one
-              :db/doc "High priority for a task in a todo list."}])
-
-(def tasks [{:db/id (d/tempid :db.part/user)
-             :task/title "Buy groceries"
+(def tasks [{:task/title "Buy groceries"
              :task/due-date #inst "2023-04-04T17:00:00.000"
              :task/completed false
-             :task/priority :task.priority/high}
+             :task/priority :high
+             :team/tasks [[:team/name "Alpha"]]}
 
-            {:db/id (d/tempid :db.part/user)
-             :task/title "Finish project report"
+            {:task/title "Finish project report"
              :task/due-date #inst "2023-04-10T23:59:59.000"
              :task/completed false
-             :task/priority :task.priority/medium}
+             :team/tasks [[:team/name "Alpha"]]}
 
-            {:db/id (d/tempid :db.part/user)
-             :task/title "Call the plumber"
+            {:task/title "Call the plumber"
              :task/due-date #inst "2023-04-05T12:00:00.000"
              :task/completed false
-             :task/priority :task.priority/low}])
+             :team/tasks [[:team/name "Beta"]]}])
 
 (def db-uri-base "datomic:mem://")
 
@@ -67,12 +47,24 @@
 
 (comment
   (def conn (recreate-db))
-
   @(d/transact conn schema)
+
+  @(d/transact conn teams)
   @(d/transact conn tasks)
+
+  (d/q '[:find ?t ?name
+         :where [?t :team/name ?name]]
+       (d/db conn))
 
   (d/q '[:find ?title
          :where [?t :task/title ?title]]
        (d/db conn))
 
+  (d/q '[:find (pull ?t [* {:team/tasks [:team/name]}])
+         :where [?t :task/title]]
+       (d/db conn))
+
+  (d/q '[:find (pull ?t [* {:team/tasks [:team/name]}])
+         :where [?t :team/name]]
+       (d/db conn))
   :rc)
