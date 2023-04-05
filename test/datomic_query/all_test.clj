eddb1956e4388b4df8fe4d4eb0db0ad9953cfc8d
Rafael Lopes <rafael84@gmail.com>
2023-04-05 07:18:52 -0300

    done?

--- test/datomic_query/all_test.clj
+++ test/datomic_query/all_test.clj
@@ -8,6 +8,37 @@
    [matcher-combinators.matchers :as m]
    [matcher-combinators.test :refer [match?]]))
 
+;; -------------------------------------------------------------------------------------------------
+;; DATABASE     In Datomic, a database comprised of a set of datoms.
+;; -------------------------------------------------------------------------------------------------
+;; DATOMS       An immutable atomic fact that represents the addition or retraction of a
+;;              relation between an entity, an attribute, a value, and a transaction.
+;;
+;;              A datom is expressed as a five-tuple:
+;;              * an entity id (E)
+;;              * an attribute (A)
+;;              * a value for the attribute (V)
+;;              * a transaction id (Tx)
+;;              * a boolean (Op) indicating whether the datom is being added or retracted
+;;
+;;              Example Datom:
+;;              E	  42
+;;              A	  :user/favorite-color
+;;              V	  :blue
+;;              Tx	1234
+;;              Op	true
+;; -------------------------------------------------------------------------------------------------
+;; ENTITIES     An entity is a set of datoms that are all about the same E.
+;;
+;;              Example Entity:
+;;              E	  A	                    V	        Tx	  Op
+;;              42	:user/favorite-color	:blue	    1234	true
+;;              42	:user/first-name	    "John"	  1234	true
+;;              42	:user/last-name	      "Doe"	    1234	true
+;;              42	:user/favorite-color	:green	  4567	true
+;;              42	:user/favorite-color	:blue	    4567	false
+;; -------------------------------------------------------------------------------------------------
+
 ;;
 ;; Find Specifications
 ;;
